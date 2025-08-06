ca186ccc56cc682b598b7217a03e27aab41ab971
2Cgrupo4 <jaahmaringa@gmail.com>
2025-08-06 12:44:19 +0000

    aula 6-7 terminar de colocar as perguntas no java

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+CriaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+CriaCartao(
+‘Animais Silvestres’,
+‘O que são animais silvestres?’,
+‘Um animal silvestre é aquele que vive naturalmente em seu habitat, não é domesticado e, geralmente, se distancia ou foge dos humanos’
+)
+CriaCartao(
+‘Animais Silvestres’,
+‘Quais os alimetos ideais para animais silvestres?’,
+‘Frutas e legumes, Proteínas, Grãos e sementes, Folhagens, entre outros’
+)
+CriaCartao(
+‘Animais Silvestres’,
+‘É permitido ter um animal silvestre como bicho de estimação no Brasil?’,
+‘Brasil, a posse de animais silvestres como bichos de estimação é regulamentada por lei. Em geral, é proibido, a menos que o animal seja proveniente de criadouros legalizados e com a devida autorização do IBAMA (Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renováveis). A compra e venda de animais silvestres sem autorização é crime ambiental.’
+)
+
