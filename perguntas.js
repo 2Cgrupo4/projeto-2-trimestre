365aa01b86ffe0682cedb6a46d96b71d8a35c108
2Cgrupo4 <jaahmaringa@gmail.com>
2025-08-20 12:32:57 +0000

    finalizado

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,33 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta,
-)
-criaCartao(
-    ‘Animais Silvestres’,
-‘O que são animais silvestres?’,
-‘Um animal silvestre é aquele que vive naturalmente em seu habitat, não é domesticado e, geralmente, se distancia ou foge dos humanos’
-)
-criaCartao(
-‘Animais Silvestres’,
-‘Quais os alimetos ideais para animais silvestres?’,
-‘Frutas e legumes, Proteínas, Grãos e sementes, Folhagens, entre outros’
-)
-criaCartao(
-‘Animais Silvestres’,
-‘É permitido ter um animal silvestre como bicho de estimação no Brasil?’,
-‘Brasil, a posse de animais silvestres como bichos de estimação é regulamentada por lei. Em geral, é proibido, a menos que o animal seja proveniente de criadouros legalizados e com a devida autorização do IBAMA (Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renováveis). A compra e venda de animais silvestres sem autorização é crime ambiental.’
+    'Animais Silvestres',
+    'O que são animais silvestres ?',
+    'Um animal silvestre é aquele que vive naturalmente em seu habitat, não é domesticado e, geralmente, se distancia ou foge dos humanos'
 )
 
-CriaCartao(
-‘Animais Silvestres’,
-‘Todos os animais silvestres são perigosos?’,
-‘Não. Embora alguns possam ser perigosos se ameaçados ou se você invadir seu espaço, a maioria dos animais silvestres evita o contato humano. O perigo real está na transmissão de doenças zoonóticas, por isso é melhor manter distância.‘
+criaCartao(
+    'Animais Silvestres',
+    'Quais os alimetos ideais para animais silvestres ?',
+    'Frutas e legumes, Proteínas, Grãos e sementes, Folhagens, entre outros'
 )
 
-function criaCartao(categoria, pergunta, resposta) {
-console.log(categoria, pergunta, resposta);
-}
+criaCartao(
+    'Animais Silvestres',
+    'É permitido ter um animal silvestre como bicho de estimação no Brasil ?',
+'Brasil, a posse de animais silvestres como bichos de estimação é regulamentada por lei.Em geral, é proibido, a menos que o animal seja proveniente de criadouros legalizados e com a devida autorização do IBAMA(Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renováveis). A compra e venda de animais silvestres sem autorização é crime ambiental.'
+)
+
+
+
+criaCartao(
+'Animais Silvestres',
+'É permitido ter um animal silvestre como bicho de estimação no Brasil ?',
+'Brasil, a posse de animais silvestres como bichos de estimação é regulamentada por lei.Em geral, é proibido, a menos que o animal seja proveniente de criadouros legalizados e com a devida autorização do IBAMA(Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renováveis). A compra e venda de animais silvestres sem autorização é crime ambiental.'
+)
+
+criaCartao(
+'Animais Silvestres',
+'Todos os animais silvestres são perigosos ?',
+'Não.Embora alguns possam ser perigosos se ameaçados ou se você invadir seu espaço, a maioria dos animais silvestres evita o contato humano.O perigo real está na transmissão de doenças zoonóticas, por isso é melhor manter distância.'
+)
+
+
